5dcbdb2b115a4a956c3e5bc038a7a90fedcdc589
RSLastra <raul.slastra@gmail.com>
2024-06-18 09:54:09 -0600

    express configuration

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
-console.log('Hola mundo!!!');
+const express = require('express');
+
+//*? crear servidor de express
+
+const app = express();
+
+
+//Rutas
+app.get('/', (req, res) => {
+    
+    res.json({
+        ok: true
+    })
+})
+
+
+//Escuchar peticiones
+app.listen(3001, () => {
+    console.log('servidor corriendo en el puerto 3001');
+});
