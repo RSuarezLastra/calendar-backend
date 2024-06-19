b72c83c478c0db313107bd0aacd23029e2526355
RSLastra <raul.slastra@gmail.com>
2024-06-19 09:36:11 -0600

    feat: reading request body

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,23 +2,37 @@ const { response } = require('express');
 
 const createUser = (req, res = response) => {
 
-    res.json({
+    const { name, email, password } = req.body;
+
+    if(!name || name.length < 2) return res.status(400).json({
+        ok: false,
+        msj: 'El nombre es requerido'
+    })
+
+    res.status(201).json({
         ok: true,
-        ms: 'Usuario registrado'
+        msj: 'Usuario registrado',
+        name,
+        email,
+        password,
     });
 }
 const loginUser = (req, res = response) => {
 
+    const { email, password } = req.body;
+
     res.json({
         ok: true,
-        ms: 'login'
+        ms: 'usuario loggeado correctamente',
+        email,
+        password,
     });
 }
 const renewToken = (req, res = response) => {
 
     res.json({
         ok: true,
-        ms: 'token renovado'
+        msj: 'token renovado'
     });
 }
 
