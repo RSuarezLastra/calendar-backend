c5af102ba8f0f219d5bc5cd000a83b0aed32bbd3
RSLastra <raul.slastra@gmail.com>
2024-06-20 09:54:47 -0600

    feat: custom middleware (field-validator)

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
+const { fieldValidator} = require('../middlewares/field-validator');
+
 const router = Router();
 
 const { createUser, loginUser, renewToken } = require('../controllers/auth');
@@ -11,6 +13,7 @@ router.post('/',
     [
         check('email', 'El email es obligatorio').isEmail(),
         check('password', 'La contraseña debe tener al menos 8 caracteres').isLength({ min: 8 }),
+        fieldValidator
     ],
     loginUser
 );
@@ -19,6 +22,7 @@ router.post('/register',
         check('name', 'El nombre es obligatorio').not().isEmpty(),
         check('email', 'El email es obligatorio').isEmail(),
         check('password', 'La contraseña debe tener al menos 8 caracteres').isLength({ min: 8 }),
+        fieldValidator
     ],
     createUser
 );
