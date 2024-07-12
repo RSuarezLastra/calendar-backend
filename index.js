9b3b540806922f32a1bb049bf1a1e6fdc9cd103f
RSLastra <raul.slastra@gmail.com>
2024-07-12 08:56:45 -0600

    feat: adding frontend build in to the public folder

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+const path = require('path')
 require('dotenv').config();
 const cors = require('cors');
 const { dbConnection } = require('./database/config');
@@ -26,6 +27,10 @@ app.use(express.json());
 //Rutas
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
+
+app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+});  
 
 
 
